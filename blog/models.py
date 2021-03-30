68e72bb7af7714ca1ad11633381dc8480f8c4593
BESSAI Mohamed Cherif <mohamedcherif.bessai@gmail.com>
2021-03-30 18:45:27 +0100

    eleventh commit

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     date = models.DateField()
-    description = models.TextField(max_length=5000)
+    description = models.TextField(max_length=10000)
 
     def __str__(self):
         return self.title
